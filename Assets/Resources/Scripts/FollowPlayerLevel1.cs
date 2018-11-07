c4b7a4b53b80154fcc21553fcda61d5d319653d6
DivineMemes <7319wgallagher@gmail.com>
2018-11-07 04:04:58 -0800

    finished more levels and added menu and end screen

---
+++ Assets/Resources/Scripts/FollowPlayerLevel1.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayerLevel1 : MonoBehaviour
+{
+    public GameObject player;
+
+	void Start ()
+    {
+		
+	}
+	
+	
+	void Update ()
+    {
+       gameObject.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, player.transform.position.z);
+	}
+}
---
+++ Assets/Resources/Scripts/PlayGame.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayGame : MonoBehaviour
+{
+    public void Play()
+    {
+        int nextScene = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(nextScene + 1);
+    }
+}
